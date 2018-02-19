3af7dfb85ce7cb62cfc09ef0f999a6393333820a
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-19 10:12:14 +0200

    Add Social Icons. Mod_01

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  // ============ Main nav-bar =============
   $('.toggler-icon').on('click', function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+  
 });
