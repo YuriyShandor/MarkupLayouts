cd0c32b7c49cb78caa9169b257e35a92c9939763
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-20 16:30:28 +0200

    Add Nav-Bars. Mod_04

--- js/nav-bars.js
+++ js/nav-bars.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // ============ Top nav-bar =============
-  // // ============ For animate =============
+  // // ============ For animate for desctop hover=============
   // $('.main-ul > li').hover(function() {
   //   $(this).children("ul").slideToggle('0.5s');
   // });
@@ -8,4 +8,20 @@ $(document).ready(function() {
   //   $(this).children("ul").slideToggle('0.5s');
   // });
 
+  $('.top-bar__toggler-icon').on('click', function() {
+    $(this).toggleClass('top-bar__toggler-icon_close');
+    $('.top-bar__inside').slideToggle('0.5s');
+  });
+
+  function topBarForTablet() {
+    if (window.matchMedia('(max-width: 991px)').matches) {
+      $('.main-ul > li').hover(function() {
+        $(this).children("ul").slideToggle('0.5s');
+      });
+      $('.main-ul > li  li').hover(function() {
+        $(this).children("ul").slideToggle('0.5s');
+      });
+    };
+  };
+  topBarForTablet();
 });
