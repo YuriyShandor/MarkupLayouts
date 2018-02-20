b9ccd9478b126449d772204e84201cfc5924113a
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-20 12:48:06 +0200

    Add Nav-Bars. Mod_02

---
+++ js/nav-bars.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // ============ Top nav-bar =============
+  // // ============ For animate =============
+  // $('.main-ul > li').hover(function() {
+  //   $(this).children("ul").slideToggle('0.5s');
+  // });
+  // $('.main-ul > li  li').hover(function() {
+  //   $(this).children("ul").slideToggle('0.5s');
+  // });
+
+});
