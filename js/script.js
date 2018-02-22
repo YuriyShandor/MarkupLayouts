48098171293dc61423b17b431c09e4c7ea892f57
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-22 13:41:22 +0200

    Add Accordion. Mod_01

--- js/script.js
+++ js/script.js
@@ -4,5 +4,22 @@ $(document).ready(function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
-  
+
+  //============== Simple accordion =========
+  $('.accordion__item-content').hide();
+  $('.accordion__item-content:eq(2)').slideDown();
+
+  $('.accordion__item-ttl').click(function() {
+  		$('.accordion__item-ttl').removeClass('accordion__item-ttl_active');
+			$(this).addClass('accordion__item-ttl_active');
+      $this = $(this);
+      $target =  $this.next();
+
+      if($this.hasClass('accordion__item-ttl_active')){
+        $('.accordion__item-content').slideUp();
+        $target.slideDown();
+      }
+
+    return false;
+  });
 });
