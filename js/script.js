2564b000416eaef16f3bec28bd3206eab4d60f5e
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-22 18:24:10 +0200

    Add Accordion. Mod_02

--- js/script.js
+++ js/script.js
@@ -8,12 +8,15 @@ $(document).ready(function() {
   //============== Simple accordion =========
   $('.accordion__item-content').hide();
   $('.accordion__item-content:eq(2)').slideDown();
+  $('.accordion__item-ttl:eq(2)').addClass('accordion__item-ttl_active');
 
   $('.accordion__item-ttl').click(function() {
   		$('.accordion__item-ttl').removeClass('accordion__item-ttl_active');
 			$(this).addClass('accordion__item-ttl_active');
+      //$(this).toggleClass('accordion__item-ttl_active');
       $this = $(this);
       $target =  $this.next();
+      //$target.slideToggle();
 
       if($this.hasClass('accordion__item-ttl_active')){
         $('.accordion__item-content').slideUp();
