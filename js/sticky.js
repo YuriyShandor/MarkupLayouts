cecd8a871e49ffd21ef9d6b17fcb654b24fc1cc0
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-24 16:52:11 +0200

    Add Sticky. Mod_06

--- js/sticky.js
+++ js/sticky.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   });
 
   // ============ Sticky btn to top =============
+
   $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
       $('#btnToTop').addClass('show');
@@ -19,5 +20,16 @@ $(document).ready(function() {
       scrollTop : 0
     },750);
     return false;
+  });
+
+  // =============== Sticky nav-bar ============
+
+  var stickyNavBar = $('.sticky-nav-bar__block').offset();
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > stickyNavBar.top) {
+      $('.sticky-nav-bar__block').addClass('stick');
+    } else {
+      $('.sticky-nav-bar__block').removeClass('stick');
+    }
   });
 });
