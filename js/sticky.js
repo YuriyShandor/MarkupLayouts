c88fb56a30ed5bd14790836fd042b89afb552307
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-25 11:06:46 +0200

    Add Sticky. Mod_07

--- js/sticky.js
+++ js/sticky.js
@@ -32,4 +32,44 @@ $(document).ready(function() {
       $('.sticky-nav-bar__block').removeClass('stick');
     }
   });
+
+  // For burger menu
+  $('.sticky-nav__toggler-icon').on('click', function() {
+    $(this).toggleClass('sticky-nav__toggler-icon_close');
+    $('.sticky-nav-bar').slideToggle('0.5s');
+  });
+
+  // For sections navigation
+  // var sections = $('.sections')
+  // , nav = $('.sticky-nav-bar')
+  // , nav_height = nav.outerHeight();
+  //
+  // $(window).on('scroll', function () {
+  //   var cur_pos = $(this).scrollTop();
+  //
+  //   sections.each(function() {
+  //     var top = $(this).offset().top - nav_height -15,
+  //     bottom = top + $(this).outerHeight() + 125;
+  //
+  //     if (cur_pos >= top && cur_pos <= bottom) {
+  //       nav.find('a').removeClass('active');
+  //       sections.removeClass('active');
+  //
+  //       $(this).addClass('active');
+  //       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+  //     }
+  //   });
+  // });
+  //
+  // nav.find('a').on('click', function () {
+  //   var $el = $(this)
+  //   , id = $el.attr('href');
+  //
+  //   $('html, body').animate({
+  //     scrollTop: $(id).offset().top - nav_height
+  //   }, 700);
+  //
+  //   return false;
+  //   });
+  // });
 });
