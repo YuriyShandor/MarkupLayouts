e694e64db74e0e253f76f1a632aac8ac88478cb7
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-25 12:13:09 +0200

    Add Sticky. Mod_08

--- js/sticky.js
+++ js/sticky.js
@@ -40,36 +40,34 @@ $(document).ready(function() {
   });
 
   // For sections navigation
-  // var sections = $('.sections')
-  // , nav = $('.sticky-nav-bar')
-  // , nav_height = nav.outerHeight();
-  //
-  // $(window).on('scroll', function () {
-  //   var cur_pos = $(this).scrollTop();
-  //
-  //   sections.each(function() {
-  //     var top = $(this).offset().top - nav_height -15,
-  //     bottom = top + $(this).outerHeight() + 125;
-  //
-  //     if (cur_pos >= top && cur_pos <= bottom) {
-  //       nav.find('a').removeClass('active');
-  //       sections.removeClass('active');
-  //
-  //       $(this).addClass('active');
-  //       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-  //     }
-  //   });
-  // });
-  //
-  // nav.find('a').on('click', function () {
-  //   var $el = $(this)
-  //   , id = $el.attr('href');
-  //
-  //   $('html, body').animate({
-  //     scrollTop: $(id).offset().top - nav_height
-  //   }, 700);
-  //
-  //   return false;
-  //   });
-  // });
+  var sections = $('.sections');
+  var nav = $('.sticky-nav');
+  var nav_height = nav.outerHeight();
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height - 100;
+      var bottom = top + $(this).outerHeight() - 350;
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      } else if (cur_pos < 1000) { // for remove class 'active' from all
+        nav.find('a').removeClass('active');
+      }
+    });
+  });
+
+  nav.find('a').on('click', function () {
+    var $el = $(this);
+    var id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - nav_height
+    }, 700);
+
+    return false;
+  });
 });
