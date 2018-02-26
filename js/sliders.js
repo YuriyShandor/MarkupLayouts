c2f40acd1a298c50b4f5b9635088e063f7d5acc7
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-26 10:59:06 +0200

    Add Sliders. Mod_02

--- js/sliders.js
+++ js/sliders.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
   // ============ Main nav-bar =============
+
   $('.toggler-icon').on('click', function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
 
-  
+  // ============ Simple slider =============
+
+  $('.simple-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   nextArrow: $(".simple-slider__arrow_next"),
+   prevArrow: $(".simple-slider__arrow_prev"),
+   dots: true,
+   dotsClass: 'simple-slider__slick-dots',
+   // autoplay: true,
+   // autoplaySpeed: 5000,
+  });
 });
