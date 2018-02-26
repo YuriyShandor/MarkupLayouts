f274ba8e8099c9b77f73c9f8d1bfeb1a21cd72d7
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-26 11:30:42 +0200

    Add Sliders. Mod_03

--- js/sliders.js
+++ js/sliders.js
@@ -15,7 +15,22 @@ $(document).ready(function() {
    prevArrow: $(".simple-slider__arrow_prev"),
    dots: true,
    dotsClass: 'simple-slider__slick-dots',
+   autoplay: true,
+   autoplaySpeed: 5000
+  });
+
+  // ============ Vertical slider =============
+
+  $('.vertical-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   vertical: true,
+   verticalSwiping: true,
+   nextArrow: $(".vertical-slider__arrow_next"),
+   prevArrow: $(".vertical-slider__arrow_prev"),
+   dots: true,
+   dotsClass: 'vertical-slider__slick-dots',
    // autoplay: true,
-   // autoplaySpeed: 5000,
+   // autoplaySpeed: 5000
   });
 });
